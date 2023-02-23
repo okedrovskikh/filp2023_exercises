0e883a9fc8b5e5d99f0eea7ed33da6c5f41c01af
ut1Puti <sololasuklina@gmail.com>
2023-02-24 00:12:36 +0500

    add vector defs

--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,28 +1,36 @@
 package exercises01
 
+import scala.math.sqrt
+
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = ???
+  def +(other: Vector): Vector = new Vector(other.x + this.x, other.y + this.y)
 
-  def -(other: Vector): Vector = ???
+  def -(other: Vector): Vector = new Vector(this.x - other.x, this.y - other.y)
 
-  def *(scalar: Double): Vector = ???
+  def *(scalar: Double): Vector = new Vector(this.x * scalar, this.y * scalar)
 
-  def unary_- : Vector = ???
+  def unary_- : Vector = new Vector(-this.x, -this.y)
 
-  def euclideanLength: Double = ???
+  def euclideanLength: Double = sqrt(this.x * this.x + this.y * this.y)
 
-  def normalized: Vector = ???
+  def normalized: Vector = {
+    val length = euclideanLength
+    if (length == 0) new Vector(0, 0) else new Vector(this.x / length, this.y / length)
+  }
 
-  override def equals(other: Any): Boolean = ???
+  override def equals(other: Any): Boolean = {
+    val vector = other.asInstanceOf[Vector]
+    this.x == vector.x && this.y == vector.y;
+  }
 
-  // Vector(x, y)
-  override def toString: String = ???
+  override def toString: String = "Vector(" + this.x.toString + ", " + this.y.toString + ")"
+
 }
 
 object Vector {
-  def fromAngle(angle: Double, length: Double): Vector = ???
+  def fromAngle(angle: Double, length: Double): Vector = new Vector(length * math.cos(angle), length * math.sin(angle))
 
-  def sum(list: List[Vector]): Vector = ???
+  def sum(list: List[Vector]): Vector = list.foldRight(new Vector(0, 0))((A, B) => new Vector(A.x + B.x, A.y + B.y))
 
-  def unapply(arg: Vector): Option[(Double, Double)] = ???
+  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }
