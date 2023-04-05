e9888fc36038cb9c3ba325da85ca692c356fb0b7
ut1Puti <sololasuklina@gmail.com>
2023-04-05 22:12:24 +0500

    Kедровских Oлег exercises06

--- exercises06/src/main/scala/exercises06/e3_transformer/Transformer.scala
+++ exercises06/src/main/scala/exercises06/e3_transformer/Transformer.scala
@@ -9,10 +9,30 @@ trait Transformer[A, B] {
 }
 
 object TransformerInstances {
-  implicit val transformer: Transformer[RawUser, User] = ???
+  implicit val transformer: Transformer[RawUser, User] = new Transformer[RawUser, User] {
+    override def toOption(a: RawUser): Option[User] =
+      for {
+        firstName  <- a.firstName
+        secondName <- a.secondName
+        id         <- a.id.toLongOption
+      } yield User(id, UserName(firstName, secondName, a.thirdName))
+
+    override def toEither(a: RawUser): Either[Error, User] =
+      for {
+        firstName  <- a.firstName.toRight(InvalidName)
+        secondName <- a.secondName.toRight(InvalidName)
+        id         <- a.id.toLongOption.toRight(InvalidId)
+      } yield User(id, UserName(firstName, secondName, a.thirdName))
+  }
 }
 
-object TransformerSyntax {}
+object TransformerSyntax {
+
+  implicit class TransformerOps[A](private val a: A) extends AnyVal {
+    def transformToOption[B](implicit ev: Transformer[A, B]): Option[B]        = ev.toOption(a)
+    def transformToEither[B](implicit ev: Transformer[A, B]): Either[Error, B] = ev.toEither(a)
+  }
+}
 
 object Examples {
   import TransformerInstances._
--- exercises06/src/main/scala/exercises06/e4_eq/Eq.scala
+++ exercises06/src/main/scala/exercises06/e4_eq/Eq.scala
@@ -4,11 +4,31 @@ trait Eq[A] {
   def eqv(a: A, b: A): Boolean
 }
 
-object Eq {}
+object EqInstances {
+  implicit val intEq: Eq[Int] = (a: Int, b: Int) => a == b
 
-object EqInstances {}
+  implicit val stringEq: Eq[String] = (a: String, b: String) => a == b
 
-object EqSyntax {}
+  implicit val boolEq: Eq[Boolean] = (a: Boolean, b: Boolean) => a == b
+
+  implicit def optionEq[A](implicit base: Eq[A]): Eq[Option[A]] =
+    (a: Option[A], b: Option[A]) =>
+      (a, b) match {
+        case (None, None)       => true
+        case (Some(x), Some(y)) => base.eqv(x, y)
+        case _                  => false
+      }
+
+  implicit def listEq[A](implicit base: Eq[A]): Eq[List[A]] = (a: List[A], b: List[A]) => a.corresponds(b)(base.eqv)
+}
+
+object EqSyntax {
+  implicit class EqOps[A](private val a: A) extends AnyVal {
+    def eqv(b: A)(implicit ev: Eq[A]): Boolean = ev.eqv(a, b)
+    def ===(b: A)(implicit ev: Eq[A]): Boolean = eqv(b)
+    def !==(b: A)(implicit ev: Eq[A]): Boolean = !eqv(b)
+  }
+}
 
 object Examples {
   import EqInstances._
@@ -20,4 +40,6 @@ object Examples {
   // 1 === "some-string" // не компилируется
   // 1 !== Some(2) // не компилируется
   List(true) === List(true) // возвращает true
+  List(true) eqv List(true) // возвращает true
 }
+
